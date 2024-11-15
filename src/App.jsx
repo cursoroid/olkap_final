83904d96ee95736e0c68a46aae040e272b7244b0
Abhi232003 <khodadeabhishek@gmail.com>
2024-11-15 19:45:41 +0530

    Logo alignment fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import Footer from './components/footer';
-import Hero from './components/hero'; // Import the Hero component
+import Footer from './components/footer/footer';
+import Hero from './components/hero/hero'; // Import the Hero component
 
 function App() {
   const [count, setCount] = useState(0);
