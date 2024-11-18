5e506b54eb20d16647851b3000eb7a90fb1f5f18
Abhi232003 <khodadeabhishek@gmail.com>
2024-11-18 17:57:53 +0530

    navbar updated

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,19 +1,31 @@
-// src/components/Navbar.jsx
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight, faBars, faTimes} from '@fortawesome/free-solid-svg-icons';
+import { ShoppingBag, User } from 'lucide-react';
+import './navbar.css';
 
 const Navbar = ({ scrolling, menuOpen, toggleMenu }) => {
   return (
     <div className={`navbar ${scrolling ? 'scrolled' : ''}`}>
       <div className="navbar-left">
-        <a href="/collection" className="navbar-item">Collection</a>
-        <a href="/store" className="navbar-item">Store</a>
+        
       </div>
+
+      {/* Center options */}
+      <div className="navbar-center">
+      <a href="/" className="navbar-item">SHOP</a>
+      <a href="/" className="navbar-item">TOP PICKS</a>
+        <a href="/" className="navbar-item">FURNITURE</a>
+        <a href="/" className="navbar-item">DECOR</a>
+        <a href="/" className="navbar-item">GALLERY</a>
+      </div>
+
       <div className="navbar-right">
-        <a href="/contact" className="navbar-item">Contact</a>
-        <a href="/about" className="navbar-item">About</a>
+        <a href="/cart" className="navbar-item"><ShoppingBag /></a>
+        <a href="/profile" className="navbar-item"><User /></a>
       </div>
+
+      
 
       {/* Hamburger Icon */}
       <div className={`hamburger ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
