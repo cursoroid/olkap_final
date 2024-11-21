22cac49c18efb195f033eed8044e055e93d63a5d
Abhi232003 <khodadeabhishek@gmail.com>
2024-11-21 13:49:11 +0530

    navbar sub menus

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import './navbar.css';
 import 'material-symbols';
 
 const Navbar = ({ scrolling, menuOpen, toggleMenu }) => {
   const shopName = "kollkap.myshopify.com"; // Replace with your shop's name
+  const [megaMenuVisible, setMegaMenuVisible] = useState(null);
+
+  const handleHover = (section) => {
+    setMegaMenuVisible(section);
+  };
+
+  const handleLeave = () => {
+    setMegaMenuVisible(null);
+  };
 
   const handleProfileClick = () => {
     const profileUrl = `https://${shopName}/account`;
     const signupUrl = `https://${shopName}/account/register`;
-
-    // Redirect to Shopify profile page
     window.location.href = profileUrl; // Shopify handles login redirection if the user is not logged in
   };
 
@@ -22,8 +29,34 @@ const Navbar = ({ scrolling, menuOpen, toggleMenu }) => {
       {/* Center options */}
       <div className="navbar-center">
         <a href={`https://${shopName}`} className="navbar-item">Shop</a>
-        <a href="/" className="navbar-item">Furniture</a>
-        <a href="/" className="navbar-item">Decor</a>
+        <div
+          className="navbar-item mega-menu-parent"
+          onMouseEnter={() => handleHover('furniture')}
+          onMouseLeave={handleLeave}
+        >
+          Furniture
+          {megaMenuVisible === 'furniture' && (
+            <div className="mega-menu">
+              <a href="/sofas" className="mega-menu-item">Sofas</a>
+              <a href="/chairs" className="mega-menu-item">Chairs</a>
+              <a href="/tables" className="mega-menu-item">Tables</a>
+            </div>
+          )}
+        </div>
+        <div
+          className="navbar-item mega-menu-parent"
+          onMouseEnter={() => handleHover('decor')}
+          onMouseLeave={handleLeave}
+        >
+          Decor
+          {megaMenuVisible === 'decor' && (
+            <div className="mega-menu">
+              <a href="/artwork" className="mega-menu-item">Artwork</a>
+              <a href="/lighting" className="mega-menu-item">Lighting</a>
+              <a href="/vases" className="mega-menu-item">Vases</a>
+            </div>
+          )}
+        </div>
         <a href="/gallery" className="navbar-item">Gallery</a>
       </div>
 
@@ -44,19 +77,19 @@ const Navbar = ({ scrolling, menuOpen, toggleMenu }) => {
         <FontAwesomeIcon icon={menuOpen ? faTimes : faBars} />
       </div>
 
-      {/* Sliding Menu */}
+      {/* Side Menu */}
       <div className={`side-menu ${menuOpen ? 'open' : ''}`}>
         <a href={`https://${shopName}`} className="side-menu-item">Shop</a>
         <a href="/" className="side-menu-item">Furniture</a>
         <a href="/" className="side-menu-item">Decor</a>
-        <a href="/" className="side-menu-item">Gallary</a>
+        <a href="/" className="side-menu-item">Gallery</a>
         <div className="login-container">
-          <button onClick={handleProfileClick} className="side-menu-item">          <span className="material-symbols-outlined">person</span>
-          Log In / Register</button>
+          <button onClick={handleProfileClick} className="side-menu-item">
+            <span className="material-symbols-outlined">person</span> Log In / Register
+          </button>
         </div>
       </div>
     </div>
-
   );
 };
 
--- src/components/navbarglob.jsx
+++ src/components/navbarglob.jsx
@@ -8,9 +8,18 @@ import logo from '../assets/global_logo.svg'; // Import the logo
 const NavbarGlob = () => {
   const [menuOpen, setMenuOpen] = useState(false);  // Track the state of the side menu
   const shopName = "kollkap.myshopify.com"; // Replace with your shop's name
+  const [megaMenuVisible, setMegaMenuVisible] = useState(null);
 
   const toggleMenu = () => {
     setMenuOpen(prevState => !prevState);  // Toggle the state when hamburger is clicked
+  };
+
+  const handleHover = (section) => {
+    setMegaMenuVisible(section);
+  };
+
+  const handleLeave = () => {
+    setMegaMenuVisible(null);
   };
 
   const handleProfileClick = () => {
@@ -31,11 +40,44 @@ const NavbarGlob = () => {
       </div>
 
       {/* Center options */}
-      <div className="navbar-glob-center">
-        <a href={`https://${shopName}`} className="navbar-glob-item">Shop</a>
-        <a href="/" className="navbar-glob-item">Furniture</a>
-        <a href="/" className="navbar-glob-item">Decor</a>
-        <a href="/" className="navbar-glob-item">Gallery</a>
+      {/* Center options */}
+      <div className="navbar-center">
+        <a href={`https://${shopName}`} className="navbar-item">Shop</a>
+        <div
+          className="navbar-item mega-menu-parent"
+          onMouseEnter={() => handleHover('furniture')}
+          onMouseLeave={handleLeave}
+        >
+          Furniture
+          {megaMenuVisible === 'furniture' && (
+            <div className="mega-menu">
+              <a href="/sofas" className="mega-menu-item">Sofas</a>
+              <a href="/chairs" className="mega-menu-item">Chairs</a>
+              <a href="/tables" className="mega-menu-item">Tables</a>
+              <a href="/sofas" className="mega-menu-item">Sofas</a>
+              <a href="/chairs" className="mega-menu-item">Chairs</a>
+              <a href="/tables" className="mega-menu-item">Tables</a>
+              <a href="/sofas" className="mega-menu-item">Sofas</a>
+              <a href="/chairs" className="mega-menu-item">Chairs</a>
+              <a href="/tables" className="mega-menu-item">Tables</a>
+            </div>
+          )}
+        </div>
+        <div
+          className="navbar-item mega-menu-parent"
+          onMouseEnter={() => handleHover('decor')}
+          onMouseLeave={handleLeave}
+        >
+          Decor
+          {megaMenuVisible === 'decor' && (
+            <div className="mega-menu">
+              <a href="/artwork" className="mega-menu-item">Artwork</a>
+              <a href="/lighting" className="mega-menu-item">Lighting</a>
+              <a href="/vases" className="mega-menu-item">Vases</a>
+            </div>
+          )}
+        </div>
+        <a href="/gallery" className="navbar-item">Gallery</a>
       </div>
 
       <div className="navbar-glob-right">
